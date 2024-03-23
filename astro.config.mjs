236cbc0fcc1de4dae6af8a09ddfa37e2d5b5fd41
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-03-23 07:57:17 +0100

    update serverless vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
